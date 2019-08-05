85048bb98e4c791eef099f39c73a432fa65177f5
Ballinette <gballini@kaliop.com>
2019-08-05 14:29:11 +0200

    prefix RNIPP env vars

--- fc-support/src/config/rnipp.ts
+++ fc-support/src/config/rnipp.ts
@@ -1,13 +1,13 @@
 export default {
-  hostname: process.env.HOSTNAME || 'rnipp.docker.dev-franceconnect.fr',
+  hostname: process.env.RNIPP_HOSTNAME || 'rnipp.docker.dev-franceconnect.fr',
   baseUrl:
-    process.env.BASE_URL ||
+    process.env.RNIPP_BASE_URL ||
     '/Brpp2IdentificationComplet/individus?rechercheType=S',
-  userId: process.env.USER_ID || '',
-  clientSiret: process.env.CLIENT_SIRET || '',
-  protocol: 'https',
+  userId: process.env.RNIPP_USER_ID || '',
+  clientSiret: process.env.RNIPP_CLIENT_SIRET || '',
+  protocol: process.env.RNIPP_PROTOCOL || 'https',
   httpsAgentConfig: {
-    rejectUnauthorized: process.env.HTTPSAGENTCONFIG || false,
+    rejectUnauthorized: process.env.RNIPP_HTTPSAGENTCONFIG || false,
   },
 
   headers() {
