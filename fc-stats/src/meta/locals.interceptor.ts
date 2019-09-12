e2a70d716207aeea615754724cc8cd0f87e801a4
sandy PIERRE <sandy.pierre@consertotech.pro>
2019-09-12 10:52:46 +0200

    Fix action and type action more readable

--- fc-stats/src/meta/locals.interceptor.ts
+++ fc-stats/src/meta/locals.interceptor.ts
@@ -22,6 +22,8 @@ export class LocalsInterceptor implements NestInterceptor {
 
     const appConfig = this.configService.get('app');
 
+    res.locals.helpers = {};
+
     res.locals.APP_ROOT = appConfig.app_root;
 
     res.locals.APP_ENVIRONMENT = appConfig.environment;
@@ -37,6 +39,51 @@ export class LocalsInterceptor implements NestInterceptor {
     res.locals.REQ_QUERY = req.query;
     res.locals.moment = moment;
     res.locals.queryString = queryString;
+
+    res.locals.helpers.MAPPINGS = {
+      action: {
+        rnippcheck: 'Redressement RNNIPP',
+        checkedtoken: 'Token vérifié',
+        authentication: 'Connexion',
+        eidas: 'Eidas',
+        consent: 'Consent',
+      },
+      typeAction: {
+        verification: 'Vérification',
+        rectifiedwithusenameonly: 'Redressement: avec le nom',
+        rectified: 'Redressement',
+        notrectifiedwithoneecho: 'Redressement : un écho',
+        divergence: 'Divergence',
+        rejectedsyntaxerrors: 'Rejeté: erreur de synthaxe',
+        notrectifiedwithmultipleecho: 'Pas de redressement: plusieurs échos',
+        notrectifiednoecho: "Pas de redressement : pas d'echo",
+        identityproviderchoice: "Choix du fournisseur d'identité",
+        initial: 'Initiale',
+        deactivateduserauthenticationattempt:
+          'Tentative de connexion avec un compte désactivé',
+        identityproviderauthentication: "Connexion au fournisseur d'identité",
+        newauthenticationquery: 'Nouvelle requête de connexion',
+        confirmAuthentication: 'Connexion confirmée',
+        conflict: 'Conflit',
+        userInfosWithoutEmailError: 'Information usager sans email',
+        nonExistantProviderAuthenticationAttempt:
+          "Tentative de connexion à un fournisseur d'identité inexistant",
+        deactivatedProviderAuthenticationAttempt:
+          "Tentative de connexion avec un fournisseur d'identité désactivé",
+        deceasedperson: 'Personne décédée',
+        xmlokbutmissingdata: 'XML ok mais données manquantes',
+        cantcallrnipp: 'RNIPP injoignable',
+      },
+    };
+
+    res.locals.helpers.getMapped = function getMapped(key) {
+      if (res.locals.helpers.MAPPINGS.action[key] !== undefined) {
+        return res.locals.helpers.MAPPINGS.action[key];
+      } else if (res.locals.helpers.MAPPINGS.typeAction[key] !== undefined) {
+        return res.locals.helpers.MAPPINGS.typeAction[key];
+      }
+      return key;
+    };
 
     return next.handle();
   }
