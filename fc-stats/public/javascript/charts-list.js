b40f138562e7a06b1655b3ff469f4d2730ad0acc
sandy PIERRE <sandy.pierre@consertotech.pro>
2019-09-17 13:10:36 +0200

    FIX how filter are diplayed on statistiques and graphiques page

--- fc-stats/public/javascript/charts-list.js
+++ fc-stats/public/javascript/charts-list.js
@@ -1,9 +1,9 @@
 import $ from 'jquery'
 
 export function chartsList(element) {
-    element.addEventListener('change', function(ev){    
+    $('.dropdown').addClass('displayed-filter');
+    element.addEventListener('change', function (ev) {
         const val = $("option:selected").val();
-        console.log(val)
         switch (val) {
             case 'ActionsFiRangeByWeek':
                 $('#fi-dropdown').removeClass('displayed-filter');
@@ -18,5 +18,5 @@ export function chartsList(element) {
         $('#granularity-dropdown').removeClass('displayed-filter');
         $('#reset-action').removeClass('displayed-filter');
         $('#bouton-filtrer').removeClass('displayed-filter');
-    },true)
+    }, true)
 }
