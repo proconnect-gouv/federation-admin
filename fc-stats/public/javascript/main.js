5846f13cd2c14674be0af020a2b976a65d4e098e
Hugues Charleux <h@chrx.io>
2019-09-30 10:35:14 +0200

    Fix fc-stats front build

--- fc-stats/public/javascript/main.js
+++ fc-stats/public/javascript/main.js
@@ -10,7 +10,6 @@ import { chartsList } from './charts-list';
 import { paginationStyle } from './pagination-style';
 import { validateEnrollment } from './validate-enrollment';
 import { comparePassword } from './compare-password';
-import { compareDate } from './compare-date';
 import { removeItem }  from './confirm-form';
 import 'lightpick/css/lightpick.css';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -29,7 +28,6 @@ const initMap = {
   validateEnrollment: validateEnrollment,
   comparePassword: comparePassword,
   removeItem: removeItem,
-  compareDate: compareDate,
 };
 
 $(document).ready(function() {
