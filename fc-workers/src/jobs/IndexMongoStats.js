3bd243262eff94d06f1c2a3c6ad57673a58a529d
Hugues Charleux <h@chrx.io>
2019-11-07 12:06:38 +0100

    [FCP-539] ETQ distrib, je veux connaitre le nombre de nouveaux comptes

--- fc-workers/src/jobs/IndexMongoStats.js
+++ fc-workers/src/jobs/IndexMongoStats.js
@@ -1,15 +1,16 @@
 import crypto from 'crypto';
+import moment from 'moment';
 import Job from './Job';
 
 class IndexMongoStats extends Job {
   static usage() {
     return `
       Usage:
-      > IndexMongoStats --count=<account|desactivated>
+      > IndexMongoStats --count=<account|desactivated|registration> --start=<date> --range=<day|week|month|year>
     `;
   }
 
-  async getMetric(metric) {
+  async getMetric(metric, start, range) {
     const { account } = this.db.models;
 
     switch (metric) {
@@ -17,7 +18,26 @@ class IndexMongoStats extends Job {
         return account.countDocuments();
       case 'desactivated':
         return account.find({ active: false }).countDocuments();
+      case 'registration':
+        const stop = IndexMongoStats.getStopDateForRange(start, range);
+        const query = {
+          $and: [
+            { createdAt: { $gte: new Date(start) } },
+            { createdAt: { $lt: new Date(stop) } },
+          ],
+        };
+        return account.find(query).countDocuments();
     }
+  }
+
+  static getStopDateForRange(start, range) {
+    return (
+      moment(start)
+        // Add given range
+        .add(1, range)
+        // Return a native javascript object
+        .format('YYYY-MM-DD')
+    );
   }
 
   static getMetricId(metric) {
@@ -29,45 +49,40 @@ class IndexMongoStats extends Job {
       .digest('hex');
   }
 
-  static getDate() {
-    const date = new Date();
-    date.setUTCHours(0);
-    date.setUTCMinutes(0);
-    date.setUTCSeconds(0);
-    date.setUTCMilliseconds(0);
-
-    return date;
-  }
-
   async run(params) {
     try {
       const log = this.container.get('logger');
 
-      log.info('Connection to database');
+      log.info(' * Connection to database');
       // (async cause connection is made on demand)
       this.db = await this.container.get('fcDatabase');
 
-      log.info('Input control');
+      log.info(' * Input control');
       const input = this.container.get('input');
-      const schema = { count: { type: 'string', mandatory: true } };
-      const { count } = input.get(schema, params);
+      const schema = {
+        count: { type: 'string', mandatory: true },
+        start: { type: 'date', mandatory: true },
+        range: { type: 'timeRange', mandatory: true },
+      };
 
-      log.info('Fetch the value we want from DB');
-      const value = await this.getMetric(count);
+      const { count, start, range } = input.get(schema, params);
 
-      log.info('build a document');
+      log.info(' * Fetch the value we want from DB');
+      const value = await this.getMetric(count, start, range);
+
+      log.info(' * build a document');
       const statsService = this.container.get('stats');
       const doc = statsService.createMetricDocument({
         key: count,
         value,
-        date: IndexMongoStats.getDate(),
-        range: 'day',
+        date: start,
+        range,
       });
 
-      log.info('Create a unique consistant id for idempotence');
+      log.info(' * Create a unique consistant id for idempotence');
       const id = IndexMongoStats.getMetricId(doc);
 
-      log.info('Save document to index');
+      log.info(' * Save document to index');
       await statsService.index(doc, 'stats', 'metric', id);
     } catch (error) {
       // Make sure we close connection in case of failure
--- fc-workers/tests/unit/jobs/IndexMongoStats.spec.js
+++ fc-workers/tests/unit/jobs/IndexMongoStats.spec.js
@@ -23,4 +23,43 @@ describe('IndexMongoStats', () => {
       expect(hashA).not.toEqual(hashB);
     });
   });
+
+  describe('getStopDateForRange', () => {
+    it('Sould return date + 1 day', () => {
+      // Given
+      const start = '2019-10-25';
+      const range = 'day';
+      // When
+      const stop = IndexMongoStats.getStopDateForRange(start, range);
+      // Then
+      expect(stop).toBe('2019-10-26');
+    });
+    it('Sould return date + 1 week', () => {
+      // Given
+      const start = '2019-10-25';
+      const range = 'week';
+      // When
+      const stop = IndexMongoStats.getStopDateForRange(start, range);
+      // Then
+      expect(stop).toBe('2019-11-01');
+    });
+    it('Sould return date + 1 month', () => {
+      // Given
+      const start = '2019-10-01';
+      const range = 'month';
+      // When
+      const stop = IndexMongoStats.getStopDateForRange(start, range);
+      // Then
+      expect(stop).toBe('2019-11-01');
+    });
+    it('Sould return date + 1 year', () => {
+      // Given
+      const start = '2019-10-25';
+      const range = 'year';
+      // When
+      const stop = IndexMongoStats.getStopDateForRange(start, range);
+      // Then
+      expect(stop).toBe('2020-10-25');
+    });
+  });
 });
