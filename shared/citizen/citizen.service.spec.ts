15af6d0ea92ef70298808f9fc088e9899875972e
Stéphane Herman <sherman@kaliopmail.com>
2020-04-07 15:35:05 +0000

    [FCAPP-60] Add correct completion for "présumés nés" identityHash generation + E2E

--- shared/citizen/citizen.service.spec.ts
+++ shared/citizen/citizen.service.spec.ts
@@ -104,4 +104,48 @@ describe('CitizenService', () => {
       });
     });
   });
+
+  describe('rectifyIfPartialBirthdate', () => {
+    it('should add "-01-01" if the brithdate is formatted "YYYY"', () => {
+      // setup
+      const birthdate = '1992';
+      const expected = '1992-01-01';
+
+      // action
+      // To test the private we must use this syntax
+      // tslint:disable-next-line: no-string-literal
+      const result = citizenService['rectifyIfPartialBirthdate'](birthdate);
+
+      // expect
+      expect(result).toStrictEqual(expected);
+    });
+
+    it('should add "-01" if the brithdate is formatted "YYYY-MM"', () => {
+      // setup
+      const birthdate = '1992-11';
+      const expected = '1992-11-01';
+
+      // action
+      // To test the private we must use this syntax
+      // tslint:disable-next-line: no-string-literal
+      const result = citizenService['rectifyIfPartialBirthdate'](birthdate);
+
+      // expect
+      expect(result).toStrictEqual(expected);
+    });
+
+    it('should not modify a date formatted "YYYY-MM-DD"', () => {
+      // setup
+      const birthdate = '1992-04-23';
+      const expected = '1992-04-23';
+
+      // action
+      // To test the private we must use this syntax
+      // tslint:disable-next-line: no-string-literal
+      const result = citizenService['rectifyIfPartialBirthdate'](birthdate);
+
+      // expect
+      expect(result).toStrictEqual(expected);
+    });
+  });
 });
