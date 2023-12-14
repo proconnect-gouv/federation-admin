8b69fa7babcba67794ada26abec4b21f7bb8cebd
Leila Abdesslam <leila.abdesslam@prestataire.modernisation.gouv.fr>
2023-12-14 16:41:11 +0100

    Revert "Merge branch 'FC-1381' into 'staging'"

--- fc-workers/src/services/FcDatabase/index.js
+++ fc-workers/src/services/FcDatabase/index.js
@@ -4,19 +4,10 @@ import './models/clientModel';
 
 class FcDatabase {
   static getInstance(config) {
-    /**
-     * Disable deprecated behavior
-     * @see https://github.com/Automattic/mongoose/issues/12666
-     */
-    mongoose.set('strictQuery', true);
-
     return mongoose.connect(config, {
       useNewUrlParser: true,
+      useCreateIndex: true,
       useUnifiedTopology: true,
-      authSource: process.env.FC_DB_DATABASE,
-      tls: true,
-      tlsCAFile: process.env.FC_DB_TLS_CA_FILE,
-      tlsAllowInvalidHostnames: false,
     });
   }
 }
--- shared/pagination/interface/index.ts
+++ shared/pagination/interface/index.ts
@@ -1,6 +1,2 @@
-/* istanbul ignore file */
-
-// Declarative file
 export * from './options-query-mongodb.interface';
 export * from './query-mongodb.interface';
-export * from './paginable-model.interface';
