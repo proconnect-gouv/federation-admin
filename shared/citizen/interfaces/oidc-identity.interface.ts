f698ab06fbbc7e261541b0e970083dd6608de444
Emmanuel Maravilha <emmanuel.maravilha@prestataire.modernisation.gouv.fr>
2024-06-03 12:17:41 +0200

    [FC-1690] Connexion à UD en passant par FranceConnect-v2

--- shared/citizen/interfaces/oidc-identity.interface.ts
+++ shared/citizen/interfaces/oidc-identity.interface.ts
@@ -1,7 +1,7 @@
 export interface IOidcIdentity {
   readonly gender: string;
   readonly family_name: string;
-  readonly preferred_username: string;
+  readonly preferred_username?: string;
   readonly given_name: string;
   readonly birthdate: string;
   readonly birthplace: string;
