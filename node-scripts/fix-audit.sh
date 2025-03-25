98f966cc4a083e32a4c1acfc89f34fa959474431
gdarquie <gaetan.darquie@beta.gouv.fr>
2025-03-25 10:36:07 +0100

    Rename fc-apps ref into proconnect-federation

--- node-scripts/fix-audit.sh
+++ node-scripts/fix-audit.sh
@@ -5,5 +5,5 @@ cd $FC_ROOT
 
 mkdir -p "$MAPPINGS_PATH"
 
-cp $FC_ROOT/fc-apps/node-scripts/mappings/create_index_business.json "$MAPPINGS_PATH/."
-cp $FC_ROOT/fc-apps/node-scripts/mappings/create_index_stats.json "$MAPPINGS_PATH/."
+cp $FC_ROOT/proconnect-exploitation/node-scripts/mappings/create_index_business.json "$MAPPINGS_PATH/."
+cp $FC_ROOT/proconnect-exploitation/node-scripts/mappings/create_index_stats.json "$MAPPINGS_PATH/."
